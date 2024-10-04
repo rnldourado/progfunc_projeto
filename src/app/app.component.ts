4a96f19b0aa50a200a1fefca6cd8b5038ac4c57c
Yovany Cunha <yovany33@gmail.com>
2024-10-03 22:56:22 -0300

    using groupby and change layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ export class AppComponent {
   reposResponse: any[] = [];
   reposResponseCopy: any[] = [];
   languageSelected: string = '';
-  options: any[] = [];
   starsCount: number = 0;
 
   issuesParams: string = '';
@@ -22,20 +21,10 @@ export class AppComponent {
 
   constructor(private appService: AppService) {}
 
-  selectState(state: string) {
-    this.selectedState = state;
-    this.issuesResponse = this.issuesResponse.filter(
-      (item: any) => item.state === state
-    );
-  }
-
   //issues
   getIssues() {
     this.appService.getIssues(this.issuesParams).subscribe((data: any) => {
-      this.options = distinct(data.items, 'state');
-
       this.issuesResponse = this.issuesResponseCopy = data.items;
-      console.log(this.issuesResponse);
     });
   }
 }
--- src/app/repository-search/repository-search.component.ts
+++ src/app/repository-search/repository-search.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { AppService } from '../app.service';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { distinct, foldSum, orderBy } from '../../utils/utils';
+import {
+  compose,
+  distinct,
+  foldSum,
+  groupBy,
+  orderBy,
+} from '../../utils/utils';
 import { MatSelectModule } from '@angular/material/select';
 import { MatListModule } from '@angular/material/list';
 import { MatDivider } from '@angular/material/divider';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-repository-search',
@@ -24,6 +32,8 @@ import { MatChipsModule } from '@angular/material/chips';
     MatListModule,
     MatDivider,
     MatChipsModule,
+    MatExpansionModule,
+    MatTabsModule,
   ],
   templateUrl: './repository-search.component.html',
   styleUrl: './repository-search.component.css',
@@ -35,6 +45,9 @@ export class RepositorySearchComponent {
   languageSelected: string = '';
   options: any[] = [];
   starsCount: number = 0;
+  groupBy: any = {};
+  groupList: any = [];
+  readonly panelOpenState = false;
 
   constructor(private appService: AppService) {}
 
@@ -52,12 +65,15 @@ export class RepositorySearchComponent {
     this.languageSelected = lang;
   }
 
+  groudByLanguage() {
+    this.groupBy = groupBy(this.reposResponse, 'language');
+  }
+
   getRepos() {
     this.appService.getRepos(this.reposParams).subscribe((data: any) => {
-      // console.log('Filtered Data:', groupBy(data.items, 'owner.login'));
-
       // console.log('forks:', foldSum(data.items, 'forks'));
       // console.log('distinct', distinct(data.items, 'language'));
+      // compose(foldSum(this.reposResponse, 'stargazers_count'), this.filter(data.items, 'language'));
 
       this.options = distinct(data.items, 'language');
 
@@ -70,6 +86,8 @@ export class RepositorySearchComponent {
       );
 
       this.starsCount = foldSum(this.reposResponse, 'stargazers_count');
+      this.groupBy = groupBy(this.reposResponse, 'language');
+      this.groupList = Object.keys(this.groupBy);
     });
   }
 }
